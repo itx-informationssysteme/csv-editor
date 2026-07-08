7e123cf41242c5bc724f710da589fffc155684ca
Andreas Hurst <andreas.hurst@itx.de>
2026-07-08 10:08:30 +0200

    fix(core): Change Version add Developer

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,6 +3,7 @@
 /* @phpstan-ignore-next-line */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'csv_editor',
+    'version' => '1.0.0',
     'description' => 'Backend CSV editor for the bwregiobus source file',
     'constraints' => [
         'depends' => [
