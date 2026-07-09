7e8ea5fbeaaafc0bf2db815503a24172200ba827
Patrik Marosváry <patrik.marosvary@itx.de>
2026-07-09 09:35:33 +0200

    chore: Updated compatible versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Backend CSV editor',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
     ],
     'autoload' => [
