b86df9b69a7dba45f9a1642fc844bd477aa05243
Andreas Hurst <andreas.hurst@itx.de>
2026-07-13 23:03:23 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,11 @@
 /* @phpstan-ignore-next-line */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 CSV Editor',
-    'version' => '3.0.0',
+    'version' => '1.0.1',
     'description' => 'Backend CSV editor',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.3.0-14.3.99',
+            'typo3' => '11.5.0-14.99.99',
         ],
     ],
     'autoload' => [
